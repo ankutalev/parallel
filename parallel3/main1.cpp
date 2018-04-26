0fa5e302b9da0cf3f654550df337b102b69e0e72
ulyssess <a.kutalev@g.nsu.ru>
2018-04-26 14:52:56 +0700

    fixed names issues

--- parallel3/main1.cpp
+++ parallel3/main1.cpp
@@ -30,13 +30,13 @@ int main(int argc, char**argv) {
     int coords[2];
     MPI_Cart_coords(twodims, rank, 2, coords);
 
-    int dims_col[2] = {0, 1};
-    int dims_row[2] = {1, 0};
+    int dims_row[2] = {0, 1};
+    int dims_column[2] = {1, 0};
 
 
-    MPI_Comm column_comm, row_comm;
-    MPI_Cart_sub(twodims, dims_col, &column_comm);
+    MPI_Comm row_comm, column_comm;
     MPI_Cart_sub(twodims, dims_row, &row_comm);
+    MPI_Cart_sub(twodims, dims_column, &column_comm);
 
 
     MPI_Datatype temp1, column, row;
@@ -54,39 +54,39 @@ int main(int argc, char**argv) {
 
 
     if (!rank) {
-        std::cout<<d[0]<<" "<<d[1]<<std::endl;
+//        std::cout<<d[0]<<" "<<d[1]<<std::endl;
 
         for (int i = 0,k=0; i < N1; ++i,k++) {
             for (int j = 0; j < N2; ++j,k++) {
                 matrixA[i*N2+j] = rand() % 15;
-                printf("%f ",matrixA[i*N2+j]);
+//                printf("%f ",matrixA[i*N2+j]);
             }
-            printf("\n");
+//            printf("\n");
         }
 //mpi irecv mpi i  recv; mpi i wait - коммуникация между строками
 
         for (int i = 0,k=0; i < N3; ++i,k++) {
             for (int j = 0; j < N4; ++j,k++) {
                 matrixB[i*N4+j] = rand() % 100;
-                printf("%f ",matrixB[i*N4+j]);
+//                printf("%f ",matrixB[i*N4+j]);
             }
-            printf("\n");
+//            printf("\n");
         }
     }
 
 
     if (coords[0] == 0)
-        MPI_Scatter(matrixB, 1, column, localB, N3 * N4 / d[1], MPI_DOUBLE, 0, column_comm);
+        MPI_Scatter(matrixB, 1, column, localB, N3 * N4 / d[1], MPI_DOUBLE, 0, row_comm);
     if (coords[1] == 0)
-        MPI_Scatter(matrixA, N1 / d[0], row, localA, N1 * N2 / d[0], MPI_DOUBLE, 0, row_comm);
+        MPI_Scatter(matrixA, N1 / d[0], row, localA, N1 * N2 / d[0], MPI_DOUBLE, 0, column_comm);
 
 
 
 
 
 
-    MPI_Bcast(localB, N3 * N4 / d[1], MPI_DOUBLE, 0, row_comm);
-    MPI_Bcast(localA, N1 * N2 / d[0], MPI_DOUBLE, 0, column_comm);
+    MPI_Bcast(localB, N3 * N4 / d[1], MPI_DOUBLE, 0, column_comm);
+    MPI_Bcast(localA, N1 * N2 / d[0], MPI_DOUBLE, 0, row_comm);
 
 
     for (int i = 0; i < N1 / d[0]; ++i)
@@ -96,18 +96,18 @@ int main(int argc, char**argv) {
             }
         }
 
-    for (int n = 0; n < size; ++n) {
-        MPI_Barrier(MPI_COMM_WORLD);
-        if (rank == n) {
-            printf("Rank == %d \n", rank);
-            for (int i = 0; i < N1 / d[0]; ++i) {
-                for (int j = 0; j < N4 / d[1]; ++j) {
-                    printf("%f ",localC[i * N4 / d[1] + j]);
-                }
-                printf("\n");
-            }
-        }
-    }
+//    for (int n = 0; n < size; ++n) {
+//        MPI_Barrier(MPI_COMM_WORLD);
+//        if (rank == n) {
+//            printf("Rank == %d \n", rank);
+//            for (int i = 0; i < N1 / d[0]; ++i) {
+//                for (int j = 0; j < N4 / d[1]; ++j) {
+//                    printf("%f ",localC[i * N4 / d[1] + j]);
+//                }
+//                printf("\n");
+//            }
+//        }
+//    }
 
     //3 bloka po 6 elementov blocki : N1/D[0], N4/d[1];
     double matrixC[N1 * N4];
@@ -120,7 +120,7 @@ int main(int argc, char**argv) {
 
 //   int displs[size] = {0,1,2,12,13,14,24,25,26}; //for 6
 //    int displs[size] = {0,1,8,9,16,17}; // for 4 process
-int displs[size];
+    int displs[size];
     for (int i = 0; i < d[0]; ++i) {
         for (int j = 0; j < d[1]; ++j) {
             displs[i*d[1]+j] = j + i*N1/d[0]*d[1];
